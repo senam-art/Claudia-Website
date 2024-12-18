ac65cb380d32a707ad316a13dd359e0022ea8f69
senam-art <sdzomeku03@gmail.com>
2024-12-18 03:25:25 +0000

    Add files via upload

---
+++ images/first.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>PHP Introduction</title>
+</head>
+<body>
+	<h1>Welcome to PHP</h1>
+	<?php
+	echo "Hello Senam";
+	echo "Today's date is". date("Y-m-d");
+	?>
+	</body>
+</html>
